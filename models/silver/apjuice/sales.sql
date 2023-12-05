078b2df525ad65b7edba3bbe8c8cb6ec60fa10a7
Alex <alex.dinsmoor@dbtlabs.com>
2023-12-05 23:22:52 +0000

    update string date field to date type

--- models/silver/apjuice/sales.sql
+++ models/silver/apjuice/sales.sql
@@ -2,7 +2,7 @@
 with records as (
     select
         saleID as id,
-        from_unixtime(ts) as order_time,
+        cast(from_unixtime(ts) as date) as order_time,
         Location as store_id,
         CustomerID as customer_id,
         location || "-" || cast(CustomerID as string) as unique_customer_id,
