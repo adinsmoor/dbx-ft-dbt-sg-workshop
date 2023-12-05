98e2d6a18bc5ced35d0c18b55647dd7c60865a08
Alex <alex.dinsmoor@dbtlabs.com>
2023-12-05 03:13:00 +0000

    update time range in date spine

--- models/semantic/metricflow_time_spine.sql
+++ models/semantic/metricflow_time_spine.sql
@@ -9,8 +9,8 @@ with days as (
     {{
         dbt_utils.date_spine(
             'day',
-            "to_date('01/01/2000','mm/dd/yyyy')",
-            "to_date('01/01/2027','mm/dd/yyyy')"
+            "to_date('01/01/2021','mm/dd/yyyy')",
+            "to_date('01/01/2022','mm/dd/yyyy')"
         )
     }}
 
