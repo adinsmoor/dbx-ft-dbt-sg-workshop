29611bd86de45ddd13ce1edd65d6a155d106feef
Rendy Sim <rendysim@propertyguru.com.sg>
2023-12-12 04:00:09 +0000

    develop bronze table

--- models/silver/apjuice/dim_products.sql
+++ models/silver/apjuice/dim_products.sql
@@ -1,8 +1,2 @@
-select
-    pk,
-    id,
-    ingredients,
-    name as product_name,
-    _fivetran_deleted,
-    _fivetran_synced
-from {{ source('apjuice', 'products') }}
+select pk, id, ingredients, name as product_name, _fivetran_deleted, _fivetran_synced
+from {{ source("apjuice", "products") }}
